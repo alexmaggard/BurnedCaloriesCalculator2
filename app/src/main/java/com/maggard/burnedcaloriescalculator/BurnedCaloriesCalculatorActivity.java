85da0cdcbc2f57f1fc45bc4d2de94680f05132ff
Alexander Maggard <amaggard09@gmail.com>
2016-09-07 18:50:49 -0700

    Initial commit

---
+++ app/src/main/java/com/maggard/burnedcaloriescalculator/BurnedCaloriesCalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.maggard.burnedcaloriescalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BurnedCaloriesCalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_burned_calories_calculator);
+    }
+}
